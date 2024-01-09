50cfd235c04a4b5f7a4daff64b1e6574267ec548
tejas-jani <tejasjani209@gmail.com>
2024-01-09 12:29:33 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,14 +17,14 @@ function App() {
     <UserContextProvider>
 
     <Routes>
-      <Route path="/" element={<Layout />}>
+      {/* <Route path="/" element={<Layout />}> */}
         <Route index element={ <div>hello</div>} />
         <Route path={"/login"} element={<LoginPage />} />
         <Route path={"/register"} element={<RegisterPage/>} />
         <Route path ={"/create"}  element={<CreatePost/>}/>
         <Route path={"/post/:id"} element={< PostPage />}/>
         <Route path="/edit/:id" element={<EditPost/>}/> 
-      </Route>
+      {/* </Route> */}
     </Routes>
 
     </UserContextProvider>
