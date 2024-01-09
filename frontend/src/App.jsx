4cac9105cad58a0a25373f6681552e1ea01f48cf
tejas-jani <tejasjani209@gmail.com>
2024-01-09 16:34:43 +0530

    some deplyment modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
  
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-// import { Layout } from "./components/layout";
-// import HomePage from "./pages/HomePage";
+import { Layout } from "./components/layout";
+import HomePage from "./pages/HomePage";
 import { UserContextProvider } from "./UserContext";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -11,22 +11,20 @@ import { CreatePost } from "./pages/CreatePost";
 import { PostPage } from "./pages/PostPage";
 import { EditPost } from "./pages/EditPost";
 function App() {
-  
-  console.log("hi" );
-console.log( import.meta.env.VITE_SERVER_BASE_URL );
+   
+
   return (
-    
     <UserContextProvider>
 
     <Routes>
-      {/* <Route path="/" element={<Layout />}> */}
-        <Route index element={ <div>hello</div>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
         <Route path={"/login"} element={<LoginPage />} />
         <Route path={"/register"} element={<RegisterPage/>} />
         <Route path ={"/create"}  element={<CreatePost/>}/>
         <Route path={"/post/:id"} element={< PostPage />}/>
         <Route path="/edit/:id" element={<EditPost/>}/> 
-      {/* </Route> */}
+      </Route>
     </Routes>
 
     </UserContextProvider>
